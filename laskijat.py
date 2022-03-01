3da6e2fe5c62b8675ce7f69ed5cce430de4d4b87
MatiasNissila12 <93642834+MatiasNissila12@users.noreply.github.com>
2022-03-01 10:39:32 +0200

    Update laskijat.py

--- laskijat.py
+++ laskijat.py
@@ -1,40 +1,46 @@
 class Laskija:
     """Luokka, joka toteuttaa eri laskutoimituksia.
-
     Julkiset metodit:
         summaa(Union[int, float], Union[int, float])
         kerro(Union[int, float], Union[int, float])
     """
 
-    def summaa(self, a, b):
-        """Palauttaa kahden luvun summan.
-
-        :param a: summan ensimmäinen luku
-        :type a: Union[int, float]
-        :param b: summan toinen luku
-        :type b: Union[int, float]
-        :return: lukujen a ja b summa
+    def summaa(self, *luvut):
+        """Palauttaa annettujen lukujen summan.
+        :param luvut: summattavat luvut
+        :type luvut: Union[list[Union[int, float]], tuple[Union[int, float]]]
+        :return: lukujen summa
         :rtype: Union[int, float]
         """
-        return sum([a, b])
+        return sum(luvut)
 
-    def kerro(self, a, b):
-        """Palauttaa kahden luvun tulon.
-
-        :param a: tulon ensimmäinen luku
-        :type a: Union[int, float]
-        :param b: tulon toinen luku
-        :type b: Union[int, float]
-        :return: lukujen a ja b tulo
+    def kerro(self, *luvut):
+        """Palauttaa annettujen lukujen tulon.
+        :param luvut: tulon ensimmäinen luku
+        :type luvut: tuple[Union[int, float]]
+        :return: lukujen tulo
         :rtype: Union[int, float]
         """
         tulo = 1
-        for luku in [a, b]:
+        for luku in luvut:
             tulo *= luku
         return tulo
 
+def argumenttien_tulostaja(**purettavat):
+    """Tulostaa annetut tiedot."""
+    for k, v in purettavat.items():
+        print(f'Argumentin "{k}" arvo on {v}.')
+    
+
 
 ### Lisää MonenLaskija ja argumenttien_tulostaja tähän.
+class MonenLaskija(Laskija):
+    """Luokka, joka toteuttaa eri laskutoimituksia mille tahansa määrälle lukuja.
+    Julkiset metodit:
+        summaa(Union[int, float], Union[int, float])
+        kerro(Union[int, float], Union[int, float])
+    """
+
 
 
 
